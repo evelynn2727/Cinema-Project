e55b8b7b4bc46b87f732c5091241a2775be1bdd8
evelynn2727 <totalnieniewybaczalne.ep@gmail.com>
2019-05-24 14:06:36 +0200

    thymeleaf

---
+++ src/main/java/pl/ewelinka/cinema/Cinema/config/ApplicationContextConfiguration.java
@@ -0,0 +1,63 @@
+package pl.ewelinka.cinema.Cinema.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+@EnableWebMvc
+public class ApplicationContextConfiguration extends WebMvcConfigurationSupport implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        // to serve static .html pages...
+        registry.addResourceHandler("/static/**").addResourceLocations("/resources/static/");
+    }
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver=new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views/thymeleaf/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        templateEngine.addDialect(new LayoutDialect());
+        //templateEngine.addDialect(springSecurityDialect());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+        viewResolver.setCache(false);
+        return viewResolver;
+    }
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+     this.applicationContext=applicationContext;
+    }
+}
--- src/main/java/pl/ewelinka/cinema/Cinema/controllers/HomeController.java
+++ src/main/java/pl/ewelinka/cinema/Cinema/controllers/HomeController.java
@@ -2,11 +2,20 @@ package pl.ewelinka.cinema.Cinema.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String homepage2(){
+    public String index(){
         return "index";
+    }
+
+    @RequestMapping
+    public ModelAndView defaultPage()
+    {
+        ModelAndView mnv = new ModelAndView();
+        mnv.setViewName("index");
+        return mnv;
     }
 }
